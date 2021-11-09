c39f3c1ca7769856264098ba071222298d40b101
serhii <Sergejanovskij71@gmail.com>
2021-11-09 08:15:27 +0200

    hw 2

--- zen.py
+++ zen.py
@@ -1,94 +1,54 @@
-from django.shortcuts import render, redirect
-from django.urls import path
-import string
 from django.core.management import execute_from_command_line
-from django.db import connection, IntegrityError
-import random
-from pathlib import Path
+from django.http import HttpResponse
+from django.urls import path
+from django.conf import settings
+import importlib.util
+
+settings.configure(
+    DEBUG=True,
+    ROOT_URLCONF=__name__,
+    SECRET_KEY='gggg'
+)
 
 
-BASE_DIR = Path(__file__).resolve().parent
-DEBUG=True,
-ROOT_URLCONF='zen'
-ALLOWED_HOSTS = ['*']
-SECRET_KEY = 'Test'
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-
-}
-]
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'urldb.sqlite3'
-    }
-}
-CREATE_URLS_TABLE = '''
-CREATE TABLE IF NOT EXISTS urls (
-url_key CHAR(5) PRIMARY KEY,
-url TEXT
-);
-'''
-INSERT_INTO_URLS = '''
-INSERT INTO urls (url_key, url)
-VALUES (%s, %s);
-'''
-SELECT_FROM_URLS = '''
-SELECT url
-FROM urls
-WHERE url_key = %s;
-'''
+TEMPLATE = """
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <div>
+     {content}
+    </div>
+  </body>
+</html>  
+    """
 
 
+def mod_handler(request, mod_name):
+    if importlib.util.find_spec(mod_name) == None:
+        return HttpResponse(status=404)
+    else:
+        list = [x for x in dir(__import__(mod_name)) if not x.startswith('_')]
+        links = ['<a href ="{}">{}</a><br>'.format(name,name) for name in list]
+        return HttpResponse(TEMPLATE.format(content=''.join(links),))
 
 
-def create_table(create_query):
-    with connection.cursor() as c:
-        c.execute(create_query)
-
-
-
-def handler(request):
-    text= {}
-    if request.method == 'POST':
-        url = request.POST['url']
-        text['url'] = url
-        if url.lower().startswith(('http:', 'https:', 'ftp:')):
-            text['is_valid'] = True
-            unique_id_found = False
-            while not unique_id_found:
-                letters = string.ascii_letters
-                digits = string.digits
-                key = ''.join(random.choice(letters + digits) for i in range(5))
-                url_key = key
-                with connection.cursor() as c:
-                    try:
-                        c.execute(INSERT_INTO_URLS, (url_key, url))
-                        unique_id_found = True
-                    except IntegrityError as e:
-                        if 'unique constraint' in e.args[0].lower():
-                            print(f" {url_key} such a key is in the database.")
-            text['url_key'] = url_key
-    return render(request, 'index.html', text)
-
-
-def url_handler(request, url_key):
-    with connection.cursor() as c:
-        c.execute(SELECT_FROM_URLS, [url_key])
-        row = c.fetchone()
-        url = row[0] if row else '/'
-        return redirect(url)
+def obj_handler(request, mod_name, obj_name):
+    link = [
+            '<pre style="word-wrap: break-word; white-space: pre-wrap;"> {} </pre>'
+            .format(getattr(__import__(mod_name), obj_name).__doc__)
+           ]
+    return HttpResponse(TEMPLATE.format(content=link))
 
 
 
 
 urlpatterns = [
-    path('', handler),
-    path('<url_key>',url_handler),
-]
+               path('doc/<mod_name>', mod_handler),
+               path('doc/<mod_name>/<obj_name>', obj_handler)
+              ]
 
-create_table(CREATE_URLS_TABLE)
+
 if __name__ == '__main__':
     execute_from_command_line()
